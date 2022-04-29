1a13896c140cd400943f1f65ec9ef7fc370df526
Vyshnav Rajesh <vyshnav2720@outlook.com>
2022-04-29 17:08:05 +0530

    hammenu test and tablet responsive

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const threeham = document.querySelector(".threeham");
 const hammenu = document.querySelector(".hammenu");
 const crossham = document.querySelector(".crossham");
+const bestselling = document.querySelector(".bestselling")
+const latest = document.querySelector(".latest")
+const browse = document.querySelector(".browse")
+const footer  = document.querySelector(".footer")
+const brandname = document.querySelector(".brandname")
 
 height= window.innerHeight + "px";
-console.log(height);
+width = window.innerWidth
+console.log(width)
 
 threeham.addEventListener("click", menuopen);
 crossham.addEventListener("click", menuclose);
@@ -13,6 +19,11 @@ function menuopen() {
     crossham.style.display = "block";
     hammenu.style.display = "flex";
     hammenu.style.height = height;
+    bestselling.style.display = "none";
+    latest.style.display = "none";
+    browse.style.display = "none";
+    footer.style.display = "none";
+    
 
 
 }
@@ -21,14 +32,35 @@ function menuclose() {
     threeham.style.display = "block";
     crossham.style.display = "none";
     hammenu.style.display = "none";
+    bestselling.style.display = "block";
+    latest.style.display = "block";
+    browse.style.display = "block";
+    footer.style.display = "block";
 }
 
 const emailbutton = document.querySelector(".emailbutton");
 
-emailbutton.addEventListener("click", noreload);
+emailbutton.addEventListener("click", noreload, false);
 
 function noreload() {
     emailbutton.innerHTML = "Joined";
     emailbutton.style.color = "var(--secondarycolor2)";
     emailbutton.style.backgroundColor = "var(--primarycolor1)";
+    return false;
 }
+
+
+if (width>=700) {
+
+    
+    brandname.innerHTML = "We Star DUst"
+}
+
+if (width<700) {
+
+    brandname.innerHTML = "WSD"
+}
+
+
+
+
